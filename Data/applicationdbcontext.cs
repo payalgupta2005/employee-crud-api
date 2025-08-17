947d609788717f80e095da71abed4d1997a52028
Vishal2612-dev <vishalgupta78644@gmail.com>
2025-08-17 15:13:15 +0530

    Add project files.

---
+++ Data/applicationdbcontext.cs
@@ -0,0 +1,14 @@
+﻿using EmployeeAdminPortal.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeAdminPortal.Data
+{
+    public class Applicationdbcontext : DbContext
+    {
+        public Applicationdbcontext(DbContextOptions<Applicationdbcontext> options) : base(options) 
+        {   
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ Models/AddEmployeeDtocs.cs
@@ -0,0 +1,10 @@
+﻿namespace EmployeeAdminPortal.Models
+{
+    public class AddEmployeeDtocs
+    {
+        public required string Name { get; set; }
+        public required string Email { get; set; }
+        public string? Phone { get; set; }
+        public double Salary { get; set; }
+    }
+}
